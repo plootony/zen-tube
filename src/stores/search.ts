d3947f6005bdfd57e47da65cda5151774af4687a
Tony <plootony.dev@gmail.com>
2024-12-19 19:28:42 +0300

    revert all 2

--- src/stores/search.ts
+++ src/stores/search.ts
@@ -6,19 +6,8 @@ export const useSearchStore = defineStore('search', () => {
   const results = ref([])
   const isPLaying = ref(false)
   const isCompact = ref(false)
-  const isLoading = ref(false)
-  const error = ref(null)
-  const searchCache = ref(new Map())
 
   const searchVideos = async (query: string) => {
-    if (searchCache.value.has(query)) {
-      results.value = searchCache.value.get(query)
-      return
-    }
-
-    isLoading.value = true
-    error.value = null
-
     if (results.value.length && isPLaying.value) {
       isCompact.value = true
     }
@@ -35,13 +24,10 @@ export const useSearchStore = defineStore('search', () => {
       results.value = response.data.items
 
       console.log(results.value)
-      searchCache.value.set(query, response.data.items)
     } catch (err) {
-      error.value = err
-    } finally {
-      isLoading.value = false
+      console.error(err)
     }
   }
 
-  return { results, isPLaying, isCompact, searchVideos, isLoading, error }
+  return { results, isPLaying, isCompact, searchVideos }
 })
